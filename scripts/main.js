36dcd060921e5c851a0fb8276c1e50cec2c6833c
Matias Di Iorio <matiasmd8610@gmail.com>
2021-02-24 22:03:03 -0300

    First Commit

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  //Close menú
+    $("main , .contact-link").on("click", function() {
+     if ($(".collapse").hasClass("show")) {
+       $('.navbar-collapse').collapse('hide');
+     }
+    });
+
+    AOS.init({
+      once: true,
+      offset: 300
+    });
+
+    //set class active to current page-----------------------
+    var current = location.pathname.split("/")[1];
+
+    if (current === "") {
+      current = "/";
+    } else if (current.indexOf(".") == -1) {
+      current = current + '.html';
+    }
+
+
+    $('#nav ul li a').each(function() {
+        var $this = $(this);
+        // we check comparison between current page and attribute redirection.
+        if ($this.attr('href') === current) {
+            $this.addClass('active');
+        }
+    });
+
+    $('#nav .contact-link').on("click", function() {
+      $('#nav ul li a').removeClass('active');
+      $(this).addClass('active');
+    });
+
+      var height = $('.navbar').height();
+
+      	$(window).on('scroll', function(){
+      		if ( $(window).scrollTop() > height ){
+      			$('.navbar').addClass('menu-fixed');
+      		} else {
+      			$('.navbar').removeClass('menu-fixed');
+      		}
+      	});
+
+
+});
